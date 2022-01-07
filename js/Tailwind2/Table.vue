532fe0058d4ea2c04b874d208a8fac04317f60a4
MICHAEL TEA <michael.tea@involve.asia>
2022-01-07 11:02:14 +0800

    exposed changeFilterValue function for slot #tableGlobalSearch

--- js/Tailwind2/Table.vue
+++ js/Tailwind2/Table.vue
@@ -14,6 +14,7 @@
         name="tableGlobalSearch"
         :search="search"
         :changeGlobalSearchValue="changeGlobalSearchValue"
+        :changeFilterValue="changeFilterValue"
       >
         <div class="flex-grow">
           <TableGlobalSearch
